58b9d4eda95e46e8a088c6a77d77bee28fcfac0a
Oliver Lewington <oliver.lewington@eviden.com>
2025-03-12 17:36:24 +0000

    Adding feature flag to disable service

--- app/index.js
+++ app/index.js
@@ -1,16 +1,29 @@
-require('./insights').setup()
 require('log-timestamp')
+require('./insights').setup()
+
 const { initialiseContainers } = require('./storage')
 const messaging = require('./messaging')
 const batching = require('./batching')
+
+const config = require('./config')
 
 process.on(['SIGTERM', 'SIGINT'], async () => {
   await messaging.stop()
   process.exit(0)
 })
 
-module.exports = (async function startService () {
-  await initialiseContainers()
-  await messaging.start()
-  await batching.start()
-}())
+const startApp = async () => {
+  if (config.processingActive) {
+    await initialiseContainers()
+    await messaging.start()
+    await batching.start()
+  } else {
+    console.info('Processing capabilities are currently not enabled in this environment')
+  }
+}
+
+(async () => {
+  await startApp()
+})()
+
+module.exports = startApp
