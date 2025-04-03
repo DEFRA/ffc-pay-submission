82fc4bdcbabd1b22ca8c95211d39e2c908882a3e
Sam Plackett <samuel.plackett@eviden.com>
2025-04-03 11:58:42 +0100

    appease the great and mighty sonar, truly the ruler of our services

--- app/batching/ledger-lines/get-description.js
+++ app/batching/ledger-lines/get-description.js
@@ -1,9 +1,10 @@
 const { BPS, FDMR } = require('../../constants/schemes')
+const BPS_FDMR_DESCRIPTION_LENGTH_TO_DROP = 6
 const MAX_DESCRIPTION_LENGTH = 60
 
 const getDescription = (schemeId, description) => {
   const eurlessDescription = description.replace(/€/g, 'EUR')
-  return (schemeId === BPS || schemeId === FDMR) ? eurlessDescription.substring(6) : eurlessDescription.substring(0, MAX_DESCRIPTION_LENGTH)
+  return (schemeId === BPS || schemeId === FDMR) ? eurlessDescription.substring(BPS_FDMR_DESCRIPTION_LENGTH_TO_DROP) : eurlessDescription.substring(0, MAX_DESCRIPTION_LENGTH)
 }
 
 module.exports = {
