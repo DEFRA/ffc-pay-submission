ac7d1c701d7489a15b52476fadd46e1b35c52d59
Oliver Lewington <oliver.lewington@atos.net>
2025-12-11 12:24:51 +0000

    Removing ','

--- app/messaging/index.js
+++ app/messaging/index.js
@@ -8,7 +8,7 @@ const start = async () => {
   for (let i = 0; i < config.submitSubscription.numberOfReceivers; i++) {
     let paymentReceiver  // eslint-disable-line
     const paymentAction = message => processPaymentMessage(message, paymentReceiver)
-    paymentReceiver = new MessageReceiver(config.submitSubscription,, paymentAction)
+    paymentReceiver = new MessageReceiver(config.submitSubscription, paymentAction)
     await paymentReceiver.subscribe(createDiagnosticsHandler(`payment-receiver-${i + 1}`))
 
     paymentReceivers.push(paymentReceiver)
