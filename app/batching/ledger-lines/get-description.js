20f9d14a4b47c1de3b3c302b6ba1c396d7308360
Sam Plackett <samuel.plackett@eviden.com>
2026-04-09 12:10:07 +0100

    appease sonar, extra safety

--- app/batching/ledger-lines/get-description.js
+++ app/batching/ledger-lines/get-description.js
@@ -3,7 +3,7 @@ const BPS_DESCRIPTION_LENGTH_TO_DROP = 6
 const MAX_DESCRIPTION_LENGTH = 60
 
 const getDescription = (schemeId, description) => {
-  const eurlessDescription = description.replaceAll(/€/g, 'EUR')
+  const eurlessDescription = description.replaceAll(/€|â‚¬/g, 'EUR')
   return (schemeId === BPS) ? eurlessDescription.substring(BPS_DESCRIPTION_LENGTH_TO_DROP) : eurlessDescription.substring(0, MAX_DESCRIPTION_LENGTH)
 }
 
