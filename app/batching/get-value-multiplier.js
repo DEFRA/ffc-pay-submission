f0bfe474ce6d276ed760c50cc1a81a6c6cbfb8a4
John Barnard <john.barnard.external@atos.net>
2026-04-22 15:01:53 +0100

    Restored comment explaining logic

--- app/batching/get-value-multiplier.js
+++ app/batching/get-value-multiplier.js
@@ -1,4 +1,6 @@
 const getValueMultiplier = (providesAccountingValues) => {
+  // For accounting purposes, the value of AP header lines and AR invoice lines are usually multiplied by -1 when provided to D365.
+  // Schemes listed in the array provide values in the correct accounting way, so should not be amended and instead passed as is.
   return providesAccountingValues ? 1 : -1
 }
 
