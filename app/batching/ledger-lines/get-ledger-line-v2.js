f6a1b141b0c2b02c6dddd5e51684095ca187eba5
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2026-09-17 11:25:46 +0100

    Add V2 journal line functions for FRPS (#176)

---
+++ app/batching/ledger-lines/get-ledger-line-v2.js
@@ -0,0 +1,40 @@
+const { convertToPounds } = require('../../currency-convert')
+const { getDescription } = require('./get-description')
+const { getValueMultiplier } = require('../get-value-multiplier')
+
+const getLedgerLineAPV2 = (invoiceLine, paymentRequest, lineId) => {
+  return [
+    'Ledger',
+    lineId,
+    invoiceLine.agreementNumber ?? paymentRequest.agreementNumber,
+    invoiceLine.accountCode,
+    invoiceLine.marketingYear ?? paymentRequest.marketingYear,
+    invoiceLine.fundCode,
+    invoiceLine.schemeCode,
+    invoiceLine.deliveryBody ?? paymentRequest.deliveryBody,
+    paymentRequest.invoiceNumber,
+    convertToPounds(invoiceLine.value),
+    getDescription(paymentRequest.schemeId, invoiceLine.description)
+  ]
+}
+
+const getLedgerLineARV2 = (invoiceLine, paymentRequest, lineId) => {
+  const valueMultiplier = getValueMultiplier(paymentRequest.providesAccountingValues)
+  return [
+    'Ledger',
+    lineId,
+    invoiceLine.agreementNumber ?? paymentRequest.agreementNumber,
+    convertToPounds((invoiceLine.value * valueMultiplier)),
+    invoiceLine.fundCode,
+    invoiceLine.schemeCode,
+    invoiceLine.marketingYear ?? paymentRequest.marketingYear,
+    invoiceLine.deliveryBody ?? paymentRequest.deliveryBody,
+    invoiceLine.accountCode,
+    getDescription(paymentRequest.schemeId, invoiceLine.description)
+  ]
+}
+
+module.exports = {
+  getLedgerLineAPV2,
+  getLedgerLineARV2
+}
